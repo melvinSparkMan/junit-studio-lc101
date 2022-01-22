a1d01035663abbac614b52ce137075f15c8c48e2
Melvin Sparkman <93449867+melvinSparkMan@users.noreply.github.com>
2022-01-22 17:14:44 -0600

    Committing the Java Web Dev: Unit Testing - Studio

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import static main.BalancedBrackets.hasBalancedBrackets;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       System.out.println("\n Balanced?: " + hasBalancedBrackets("]Some String["));
+    }
+}
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.junit.Test;
 
+import static main.BalancedBrackets.hasBalancedBrackets;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -11,6 +12,38 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void areThereLeftAndRightBrackets() {
+        assertEquals(true, hasBalancedBrackets("[Some String]"));
+    }
+    @Test
+    public void missingABracket() {
+        assertFalse(hasBalancedBrackets("[Some String"));
+    }
+    @Test
+    public void bracketsInsideOfBrackets() {
+        assertEquals(true, hasBalancedBrackets("[Some [] String]"));
+    }
+    @Test
+    public void multiplePairsOfBrackets() {
+        assertEquals(true, hasBalancedBrackets("[Some] [] [String]"));
+    }
+    @Test
+    public void onlyLeftOneBracket() {
+        assertFalse(hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyRightOneBracket() {
+        assertFalse(hasBalancedBrackets("]"));
+    }
 
+    @Test
+    public void stringBeforeBrackets() {
+        assertTrue(hasBalancedBrackets("Some String[]"));
+    }
+    @Test
+    public void stringAfterBrackets() {
+        assertTrue(hasBalancedBrackets("[]Some String"));
+    }
 
 }
